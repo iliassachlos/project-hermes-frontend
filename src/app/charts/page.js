4130263f3e92fd797d2d00f8d64986b5e25ab87c
AlexaderLoco <alexlocopap@gmail.com>
2024-05-27 20:27:30 +0300

    Added dropdown list to let user decide on the date range of the chart data.

--- src/app/charts/page.js
+++ src/app/charts/page.js
@@ -1,5 +1,6 @@
 'use client'
-import {Chip, Spinner} from "@nextui-org/react";
+import React from "react";
+import {Button, Chip, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Spinner} from "@nextui-org/react";
 import PieChart from "@/components/charts/pie-chart";
 import axios from "axios";
 import {useEffect, useState} from "react";
@@ -11,16 +12,16 @@ function ChartsPage() {
 
     const [sentimentScoreDistribution, setSentimentScoreDistribution] = useState(null);
     const [categoryDistribution, setCategoryDistribution] = useState(null);
-    const [sourceDistribution, setSourceDistribution]= useState(null);
+    const [sourceDistribution, setSourceDistribution] = useState(null);
 
 
     useEffect(() => {
-        fetchSentimentData();
+        fetchSentimentData(7);
     }, []);
 
-    async function fetchSentimentData() {
+    async function fetchSentimentData(days) {
         try {
-            const response = await axios.post('http://localhost:8083/api/elastic/chart')
+            const response = await axios.post('http://localhost:8083/api/elastic/chart', {days: days})
             setCategoryDistribution(response.data.categoryDistribution)
             setSentimentScoreDistribution(response.data.sentimentScoreDistribution)
             setSourceDistribution(response.data.sourceDistribution)
@@ -48,9 +49,29 @@ function ChartsPage() {
             }
             {!isLoading && !error && (
                 <div className="flex flex-col md:grid md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-2 gap-y-8">
+                    <Dropdown>
+                        <DropdownTrigger>
+                            <Button
+                                variant="solid"
+                            >
+                                Set Desired Date Range
+                            </Button>
+                        </DropdownTrigger>
+                        <DropdownMenu
+                            aria-label="Action event example"
+                            onAction={(key) => fetchSentimentData(key)}
+                        >
+                            <DropdownItem key="3">3 days ago</DropdownItem>
+                            <DropdownItem key="7">A week ago</DropdownItem>
+                            <DropdownItem key="30">A month ago</DropdownItem>
+                            <DropdownItem key="90">3 month ago</DropdownItem>
+                            <DropdownItem key="180">6 month ago</DropdownItem>
+                            <DropdownItem key="365">A year ago</DropdownItem>
+                        </DropdownMenu>
+                    </Dropdown>
                     <PieChart categoryDistribution={categoryDistribution} aggregationType="Categories"/>
                     <BarChart sentimentScoreDistribution={sentimentScoreDistribution} aggregationType="Sentiment"/>
-                    <PieChart categoryDistribution={sourceDistribution} aggregationType="Sources" />
+                    <PieChart categoryDistribution={sourceDistribution} aggregationType="Sources"/>
                 </div>
             )}
         </div>
